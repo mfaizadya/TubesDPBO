847ab6614029e68e92c3002c1fbc238f5938f591
mfaizadya <153928410+mfaizadya@users.noreply.github.com>
2024-12-02 19:36:36 +0700

    Add files via upload

---
+++ AplikasiStreaming/src/main/java/com/mycompany/aplikasistreaming/AplikasiStreaming.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplikasistreaming;
+
+/**
+ *
+ * @author Faiz
+ */
+public class AplikasiStreaming {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

155589e0a61f36d43f20dffc845ac361dc62203e
Frizam <frizammaulana07@gmail.com>
2024-12-04 00:02:44 +0700

    Update AplikasiStreaming.java

--- AplikasiStreaming/src/main/java/com/mycompany/aplikasistreaming/AplikasiStreaming.java
+++ AplikasiStreaming/src/main/java/com/mycompany/aplikasistreaming/AplikasiStreaming.java
@@ -11,6 +11,37 @@ package com.mycompany.aplikasistreaming;
 public class AplikasiStreaming {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        // main Recommendation
+        List<String> allFilms = Arrays.asList("Bullet Train", "Rush Hour", "Scream", "Friday The 13th");// daftar film
+
+        // pemetaan genre ke film
+        Map<String, List<String>> genreToFilms = new HashMap<>();
+        genreToFilms.put("Action", Arrays.asList("Bullet Train", "Rush Hour"));
+        genreToFilms.put("Horror", Arrays.asList("Scream", "Friday The 13th"));
+        genreToFilms.put("Comedy", Arrays.asList("Bullet Train", "Rush Hour"));
+
+        // membuat objek GenreRecommendation
+        GenreRecommendation recommendation = new GenreRecommendation(allFilms, genreToFilms);
+
+        // menampilkan semua film
+        recommendation.showAllRecommendedFilms();
+
+        // menampilkan rekomendasi berdasarkan genre
+        recommendation.showRecommendations("Action");
+        recommendation.showRecommendations("Horror");
+        recommendation.showRecommendations("Comedy");
+        recommendation.showRecommendations("Drama");
+        
+        // main CastToDevice
+        CastToDevice castToDevice = new CastToDevice("TV");
+
+        castToDevice.checkCastingStatus(); // cek status awal
+        castToDevice.startCasting();       // memulai casting
+        castToDevice.checkCastingStatus(); // cek status saat casting
+        castToDevice.startCasting();       // coba mulai casting lagi
+        castToDevice.stopCasting();        // hentikan casting
+        castToDevice.checkCastingStatus(); // cek status setelah berhenti
+        
     }
 }

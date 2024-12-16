e5da1018157aef6bc6f0b80da26511ed5ff8560f
mfaizadya <153928410+mfaizadya@users.noreply.github.com>
2024-12-16 14:42:30 +0700

    Update AplikasiStreaming.java

--- AplikasiStreaming/src/main/java/com/mycompany/aplikasistreaming/AplikasiStreaming.java
+++ AplikasiStreaming/src/main/java/com/mycompany/aplikasistreaming/AplikasiStreaming.java
@@ -12,7 +12,56 @@ import java.util.*;
 public class AplikasiStreaming {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        UserManager userManager = new UserManager();
+
+        while (true) {
+            System.out.println("===== APLIKASI STREAMING FILM =====");
+            System.out.println("Selamat Datang!");
+            System.out.println("Pilih menu berikut:");
+            System.out.println("1. Sign Up");
+            System.out.println("2. Sign In");
+            System.out.println("3. Exit");
+            System.out.println("===================================");
+            System.out.print("Pilihan: ");
+
+            String choice = scanner.nextLine();
+            switch (choice) {
+                case "1":
+                    try {
+                        System.out.print("Masukkan email    : ");
+                        String email = scanner.nextLine();
+                        System.out.print("Masukkan username : ");
+                        String username = scanner.nextLine();
+                        System.out.print("Masukkan password : ");
+                        String password = scanner.nextLine();
+
+                        userManager.registerUser(email, username, password);
+                    } catch (IllegalArgumentException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+
+                case "2":
+                    System.out.print("Masukkan email/username : ");
+                    String identifier = scanner.nextLine();
+                    System.out.print("Masukkan password       : ");
+                    String password = scanner.nextLine();
+
+                    userManager.loginUser(identifier, password);
+                    break;
+
+                case "3":
+                    System.out.println("@ Terima kasih telah menggunakan aplikasi!");
+                    scanner.close();
+                    return;
+
+                default:
+                    System.out.println("[Error]: Silahkan masukkan nomor menu yang benar!");
+                    break;
+            }
+        }
+
         // main Recommendation
         List<String> recFilms = Arrays.asList("Bullet Train", "Rush Hour", "Scream", "Friday The 13th");// daftar film
 

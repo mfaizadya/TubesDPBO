a93bbaf2c1d526bfd0430bdceded985b4be5f309
tahtafa <tahtaccnt@gmail.com>
2024-12-17 18:17:33 +0700

    Update WatchPartyTest.java

--- AplikasiStreaming/src/main/java/com/mycompany/aplikasistreaming/WatchPartyTest.java
+++ AplikasiStreaming/src/main/java/com/mycompany/aplikasistreaming/WatchPartyTest.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package watchparty;
 
@@ -8,32 +8,35 @@ package watchparty;
  *
  * @author asus
  */
-public class PrivateWatchParty extends WatchParty {
-    
-    private int invitedUsers;
-    
-    public PrivateWatchParty(String partyID, String hostUser, int participants, String startTime, int invitedUsers) {
-        super(partyID, hostUser, participants, startTime);
-        this.invitedUsers = invitedUsers;
-    }
-    
-    public int getInvitedUsers() {
-        return invitedUsers;
-    }
+public class WatchPartyTest {
 
-    public void setInvitedUsers(int invitedUsers) {
-        this.invitedUsers = invitedUsers;
-    }
-    
-    public void inviteUsers(int users) throws Exception {
-        if (users < 0) {
-            throw new Exception("jumlah pengguna yang diundang tidak valid.");
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try {
+            
+            WatchParty watchParty = new WatchParty("WP001", "Alice", 49, "2024-12-12 18:00");
+            watchParty.createWatchParty();
+            watchParty.joinParty("Bob");
+            watchParty.joinParty("John");
+            watchParty.endParty();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
-        invitedUsers += users;
-        System.out.println(users + " pengguna sudah diundang, total pengguna: " + invitedUsers);
-    }
 
-    public void lockParty() {
-        System.out.println("party ini terkunci, tidak ada pengguna yang dapat masuk lagi");
+        try {
+            
+            PrivateWatchParty privateParty = new PrivateWatchParty("PWP001", "Charlie", 5, "2024-12-12 20:00", 3);
+            privateParty.createWatchParty();
+            privateParty.inviteUsers(2);
+            privateParty.inviteUsers(-1);
+            privateParty.lockParty();
+            privateParty.endParty();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
+    
 }

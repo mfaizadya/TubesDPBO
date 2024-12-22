3c5479462f0a1592832cad218a3bcbf47420ed9f
admiralmanan <fikricrb001@gmail.com>
2024-12-22 21:15:58 +0700

    Update filmTest.java

--- AplikasiStreaming/src/main/java/com/mycompany/aplikasistreaming/filmTest.java
+++ AplikasiStreaming/src/main/java/com/mycompany/aplikasistreaming/filmTest.java
@@ -14,8 +14,14 @@ public class filmTest {
         Documentary documentary = new Documentary("D001", "Planet Earth", "Nature", 2006, 90, 9.4, "url_here", "Wildlife", "David Attenborough");
         Movie movie = new Movie("M001", "Avengers: Endgame", "Action", 2019, 181, 8.4, "url_here", true);
 
+        // Simulasi menonton film yang ada
         series.watch("The Mandalorian", 1, 1);
         documentary.watch("Planet Earth", "Wildlife");
         movie.watch("Avengers: Endgame", true, "English");
+
+        // Simulasi menonton film yang tidak ada
+        series.watch("", 1, 1);  // Exception: Film tidak ditemukan!
+        documentary.watch("", "");  // Exception: Film atau subjek tidak valid!
+        movie.watch("", true, "English");  // Exception: Film tidak ditemukan!
     }
 }
